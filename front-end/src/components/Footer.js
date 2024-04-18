96d3a4f265327639bb47b756c462048b9180e23e
Taufiq <taufiq9.2004@gmail.com>
2024-04-18 20:26:16 +0530

    tailwind setup

--- front-end/src/components/Footer.js
+++ front-end/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className='bg-black text-white p-1'>Footer</div>
   )
 }
 
--- front-end/src/components/Nav.js
+++ front-end/src/components/Nav.js
@@ -4,21 +4,21 @@ import { Link } from 'react-router-dom'
 const Nav = () => {
   return (
     <div>
-      <nav>
-        <ul>
-          <li>
+      <nav className="">
+        <ul className="flex bg-black text-white ">
+          <li className="p-1 m-1">
             <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="p-1 m-1">
             <Link to="/add">Add</Link>
           </li>
-          <li>
+          <li className="p-1 m-1">
             <Link to="/update">Update</Link>
           </li>
-          <li>
+          <li className="p-1 m-1">
             <Link to="/profile">Profile</Link>
           </li>
-          <li>
+          <li className="p-1 m-1">
             <Link to="/logout">LogOut</Link>
           </li>
         </ul>
