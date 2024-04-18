37447bcd4ec56e0639d84b0459bbe77d9f9c2718
Taufiq <taufiq9.2004@gmail.com>
2024-04-18 19:59:02 +0530

    route setup

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,12 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import Add from './components/Add';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      hello world
-    </div>
+    <RouterProvider router={AppRouter}/>
   );
 }
+
+const AppRouter = createBrowserRouter([
+  {
+    path:"/",
+    element:<Home />
+  },
+  {
+    path:"/add",
+    element: <Add />
+  },
+  {
+    path:"/update",
+    element: <div>update </div>
+  }
+])
 
 export default App;
