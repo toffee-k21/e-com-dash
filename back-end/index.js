10e9e1dc4536fe57c5e060fc9d5015894fae8acc
Taufiq <taufiq9.2004@gmail.com>
2024-04-21 16:09:35 +0530

    mongo setup

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose")
 
 const app = express();
+const connectDb = ()=>{
+    mongoose.connect("mongodb://localhost:27017/urlShortner")
+const urlSchema = new mongoose.Schema({})
+const Url = mongoose.model("url",urlSchema)
+return Url
+}
+// const entry = connectDb() 
+const fetchData = async(Url) =>{
+const data = await Url.find({})
+console.log(data)
+}
+
+fetchData(connectDb())
 
 app.get("/",(req,res)=>{
     res.send("app is working")
--- front-end/src/pages/Sign.js
+++ front-end/src/pages/Sign.js
@@ -15,7 +15,7 @@ const Sign = () => {
     <div className='m-auto w-1/2 bg-gray-100 my-10'>
         <h1 className='text-xl font-semibold m-7'>Sign Up</h1>
         <div>
-            <input name="name" onChange={(e)=>setName(e.target.value)} className='border-[1px] border-black p-1 m-7 block' type="text"/>
+            <input name="name" value={name} onChange={(e)=>setName(e.target.value)} className='border-[1px] border-black p-1 m-7 block' type="text"/>
             <input name="email" onChange={(e)=>setEmail(e.target.value)} className='p-1 m-7 block' type="email"/>
             <input name="password" onChange={(e)=>setPassword(e.target.value)}className='p-1 m-7 block' type="password"/>
             <button onClick={handleSign} className='bg-black text-white p-1 m-7 '>Sign Up</button>
