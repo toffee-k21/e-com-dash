09a72a3abdc8ba7ecb27baca8723552d21e52d51
Taufiq <taufiq9.2004@gmail.com>
2024-04-23 23:12:53 +0530

    login

--- back-end/index.js
+++ back-end/index.js
@@ -21,6 +21,23 @@ app.post("/register", (req,res )=>{
     // res.send("Hogya send to database")
     // you can't use 2 res.send at a single req
     res.send({message:"this is from data base",...req.body})
+    // insert = insert.toObject();
+    // delete insert.password
+    // res.send(insert)
+    })
+
+    app.post("/login",async (req,res)=>{
+        if(req.body.password && req.body.email){
+            const result = await User.findOne(req.body).select("-password")//this .select is used for removing password from that json string. && it is applicable for findOne.
+// note
+            // diff bw find(returns : array of objects toh agr kuch nhi milega toh [] ye return krega) and findOne(returns one objects .ie document in mongoDb)
+            if(result){
+                res.send(result);
+            }
+            else{
+                res.send({result:"user not found"});
+            }
+        }else{res.send({result:"Invalid credentials"});}
     })
 
 
