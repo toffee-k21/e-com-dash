44a320c0782baa08da014073063d715c6562569e
Taufiq <taufiq9.2004@gmail.com>
2024-04-25 15:15:42 +0530

    add product frontend

--- front-end/src/pages/Add.js
+++ front-end/src/pages/Add.js
@@ -1,9 +1,69 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Add = () => {
-  return (
-    <div>Add</div>
-  )
-}
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [company, setCompany] = useState("");
+  const [category, setCategory] = useState("");
 
-export default Add
+  const handleAdd = () => {
+    console.log(name, company, category, price);
+  };
+  return (
+    <div className="my-11 flex justify-center">
+      <div className="w-1/2">
+        <h1 className="text-2xl font-semibold">Add a product</h1>
+        <div className="flex flex-col">
+          <div>
+            <label className="m-2 block">Enter product name</label>
+            <input
+              value={name}
+              className="m-2 border-[1px] w-full p-[6px]"
+              onChange={(e) => setName(e.target.value)}
+              type="text"
+              placeholder="ex: I Phone 15 pro"
+            />
+          </div>
+          <div>
+            <label className="m-2 block">Enter product price</label>
+            <input
+              className="m-2 border-[1px] w-full p-[6px]"
+              onChange={(e) => setPrice(e.target.value)}
+              type="text"
+              placeholder="ex: 1400"
+            />
+          </div>
+          <div>
+            <label className="m-2 block">Enter Company</label>
+            <input
+              className="m-2 border-[1px] w-full p-[6px]"
+              onChange={(e) => setCompany(e.target.value)}
+              type="text"
+              placeholder="ex: Apple"
+            />
+          </div>
+          <div>
+            <label className="m-2 block">Select category</label>
+            <select
+              className=" bg-black text-white m-2 p-[6px]"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+            >
+              <option value="electronics">Electronics</option>
+              <option value="book">Book</option>
+              <option value="cloth">Clothing</option>
+            </select>
+          </div>
+          <button
+            className="bg-black text-white p-2 rounded-md m-2"
+            onClick={handleAdd}
+          >
+            Add to store
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Add;
