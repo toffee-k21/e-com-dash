c678b5753c7a2bf1d9f2cc8ff15b16f1fa15c274
Taufiq <taufiq9.2004@gmail.com>
2024-04-26 10:44:31 +0530

    frontend fetch users products

--- front-end/src/pages/Update.js
+++ front-end/src/pages/Update.js
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import ProductCard from '../components/ProductCard'
 
 const Update = () => {
+
+const [ userProducts, setUserProducts ] = useState([])
+
+  const fetchUserProducts = async(userId) =>{
+let result = await fetch ( `http://localhost:5000/update/${userId}`)
+result = await result.json();
+setUserProducts(result)
+console.log(result)
+  }
+
+useEffect(()=>{
+  let userId = localStorage.getItem("user")
+  userId = JSON.parse(userId)._id
+fetchUserProducts(userId)
+},[])
+
   return (
-    <div>Update</div>
+    <div className='m-11'>
+    <h1 className='text-2xl font-semibold'>Products</h1>
+    <div className='flex'>
+        {userProducts.map((r)=><ProductCard details={r}/>)}
+    </div>
+</div>
   )
 }
 
