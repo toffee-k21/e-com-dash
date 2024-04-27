c94f9d2244d1a80ea4baf47afa851e79093be26f
Taufiq <taufiq9.2004@gmail.com>
2024-04-28 00:08:30 +0530

    fix bug

--- front-end/src/pages/UpdateForm.js
+++ front-end/src/pages/UpdateForm.js
@@ -6,7 +6,7 @@ const UpdateFrom = () => {
   const [price, setPrice] = useState("");
   const [company, setCompany] = useState("");
   const [category, setCategory] = useState("electronics");
-  const [prefill,setPrefill] = useState({})
+  // const [prefill,setPrefill] = useState(null)
   const navigate = useNavigate()
 
 
@@ -23,21 +23,21 @@ const UpdateFrom = () => {
     })
     const data = await fetchPrefill.json();
     console.log(data)
-    setPrefill(data)
+    // setPrefill(data)
     // console.log(prefill)
-    // if (prefill != null){
-    //     setName(prefill.productName);
-    //     setPrice(prefill.price);
-    //     setCompany(prefill.company);
-    //     setCategory(prefill.category);
-    // }
+
+        setName(data.productName);
+        setPrice(data.price);
+        setCompany(data.company);
+        setCategory(data.category);
+
   }
   useEffect(()=>{
 handlePrefill()
   },[])
   // console.log(user)
   const handleUpdate = async () => {
-    // console.log(name, company, category, price , user._id);
+    console.log(name, company, category, price , user._id);
     const updateProduct = await fetch("http://localhost:5000/update/"+id,{
       method:"PATCH",
       body:JSON.stringify({
@@ -55,7 +55,7 @@ handlePrefill()
     navigate("/update")
   };
 
-  console.log(prefill)
+  // console.log(prefill)
 
   return (
     <div className="my-11 flex justify-center">
@@ -64,8 +64,10 @@ handlePrefill()
         <div className="flex flex-col">
           <div>
             <label className="m-2 block">Enter product name</label>
+           
             <input
-              value={name == '' ? prefill.productName: name}
+            value={name}
+            // value= {name == '' ? prefill.productName: name}
               className="m-2 border-[1px] w-full p-[6px]"
               onChange={(e) => setName(e.target.value)}
               type="text"
@@ -76,7 +78,8 @@ handlePrefill()
           <div>
             <label className="m-2 block">Enter product price</label>
             <input
-             value={price == '' ? prefill.price: price}
+            value={price}
+            //  value={price == '' ? prefill.price: price}
               className="m-2 border-[1px] w-full p-[6px]"
               onChange={(e) => setPrice(e.target.value)}
               type="text"
@@ -87,7 +90,8 @@ handlePrefill()
           <div>
             <label className="m-2 block">Enter Company</label>
             <input
-             value={company == '' ? prefill.company: company}
+            value={company}
+            //  value={company == '' ? prefill.company: company}
               className="m-2 border-[1px] w-full p-[6px]"
               onChange={(e) => setCompany(e.target.value)}
               type="text"
@@ -99,7 +103,8 @@ handlePrefill()
             <label className="m-2 block">Select category</label>
             <select
               className=" bg-black text-white m-2 p-[6px]"
-              value={category == '' ? prefill.category: category}
+              value={category}
+              // value={category == '' ? prefill.category: category}
               onChange={(e) => setCategory(e.target.value)}
             >
               <option value="electronics">Electronics</option>
